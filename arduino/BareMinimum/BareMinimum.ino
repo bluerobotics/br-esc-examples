59b0a1b4343883b2ff5fb8c188472c458deff198
Rustom Jehangir <rustyj110@gmail.com>
2015-04-28 23:27:03 -0700

    Added bare minimum example code.

---
+++ arduino/BareMinimum/BareMinimum.ino
@@ -0,0 +1,52 @@
+/* Blue Robotics Example Code
+-------------------------------
+ 
+Title: Bare Minimum for ESC Control (Arduino)
+
+Description: This example code shows the bare minimum to get up and 
+running with the Blue Robotics Basic ESC and BlueESC using PWM signals. 
+
+The code is designed for the Arduino Uno board and can be compiled and 
+uploaded via the Arduino 1.0+ software.
+
+-------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2015 Blue Robotics Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+-------------------------------*/
+
+#include <Servo.h>
+
+byte servoPin = 9;
+Servo servo;
+
+void setup() {
+  servo.attach(servoPin);
+
+  servo.writeMicroseconds(1500); // send "stop" signal to ESC.
+  delay(1000); // delay to allow the ESC to recognize the stopped signal
+}
+
+void loop() {
+  int signal = 1700; // Set signal value, which should be between 1100 and 1900
+
+  servo.writeMicroseconds(signal); // Send signal to ESC.
+}
---
+++ arduino/BareMinimum/README.md
@@ -0,0 +1,14 @@
+Bare Minimum for ESC Control
+============================
+
+##Description
+
+This example provides the bare minimum code required to get started with the Blue Robotics Basic ESC or BlueESC and an Arduino microcontroller. The code sets up an Arduino pin to output a PWM (servo) signal and then initializes the ESC with a 1500 microsecond signal. Once initialized, the signal is set to a different value so that the motor will start moving.
+
+##Wiring
+
+*Basic ESC:* The Basic ESC signal cables have three wires, brown is ground and orange is the signal. We do not need the center wire. Using jumper wires, connect the brown ground pins to the Arduino *GND* pins (through breadboard if necessary). Connect the orange signal wire to Arduino pin 9.
+
+*BlueESC:* The BlueESC signal cables have four wires, but all you need is ground (black) and signal (red or yellow). Connect the black ground wire to the Arduino *GND* pins (through breadboard if necessary). Connect the signal wire to Arduino pin 9.
+
+Supply power to the ESCs via the red and black cables.
